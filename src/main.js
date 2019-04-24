067c9c5dc038d1e8f7843a76346a1a520c570bac
Yin Yiling <yiling.yin@hp.com>
2019-04-24 10:56:32 +0800

    test

--- src/main.js
+++ src/main.js
@@ -3,6 +3,4 @@ import { version } from '../package.json';
 
 export default function () {
   console.log(foo + version);
-  console.log('this is a new version!')
-  console.log('this is a new version again!')
 }
