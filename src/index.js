0c8817dbe5545cc060ae84d851edb5a5367f5ed9
Yin Yiling <yiling.yin@hp.com>
2019-07-26 16:23:36 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import * as R from 'ramda'
 
-import styles from './styles.css'
-
 export const testR = R.join('||')
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
