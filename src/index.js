d650bca92fea9d19a76b4ab125252faad6905077
Yin Yiling <yiling.yin@hp.com>
2019-07-29 17:17:11 +0800

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import * as Use from './use'
 import * as Utils from './utils'
 
 export const DataStatus = Helper.DataStatus
+export const createStatusSelector = Helper.createStatusSelector
+export const createDataSelector = Helper.createDataSelector
 export const history = StoreHelper.history
 export const buildStore = StoreHelper.buildStore
 
